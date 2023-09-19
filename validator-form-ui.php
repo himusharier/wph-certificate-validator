7c46279eb0d36a4c1ee131513d9d0c02c1c5a195
razonsharier <razonsharier@gmail.com>
2023-09-20 01:34:08 +0600

    user site front-end design complete

--- validator-form-ui.php
+++ validator-form-ui.php
@@ -1,7 +1,21 @@
 <div class="wphcv-alpha-container">
-    <p>Enter Certificate ID:</p>
-    <div class="wphcv-input-form">
-        <input type="text" name="my_search_term" id="my-search-term">
-        <input type="submit" value="search" name="search">
-    </div>
+    <p class="wphcv-form-heading">Enter Certificate ID:</p>
+    <form class="wphcv-input-form">
+	    <input placeholder="Certificate ID" type="text" id="certificateId" required>
+	    <button type="submit" class="wphcv-submitButton" id="submitBtn">Validate &crarr;</button>
+	</form>
+	<div class="wphcv-certificate-details">
+		<div class="wphcv-certificate-details-heading found">
+			<img src="<?php echo plugin_dir_url( __FILE__ );?>assets/verified.png">
+			<p><a style="font-weight: bolder;">Congratulations !</a><br/> You are verified.</p>
+		</div>
+		<div class="wphcv-certificate-details-body">
+			<a><a style="font-weight: bolder;">Name:</a> Sharier Himu</a><br/>
+			<a><a style="font-weight: bolder;">Issue Date:</a> 19-09-2023</a>
+		</div>
+		<!-- <div class="wphcv-certificate-details-heading error">
+			<img src="<?php echo plugin_dir_url( __FILE__ );?>assets/not-verified.png">
+			<p><a style="font-weight: bolder;">Not Found !</a><br/> Invalid Certificate ID.</p>
+		</div> -->
+	</div>
 </div>
