a49e5c334c3681ad9f036fdc884a8a64d68b0ad3
razonusharier <razonsharier@gmail.com>
2023-09-23 01:58:25 +0600

    plugin directory and files restructured

--- wph-certificate-validator.php
+++ wph-certificate-validator.php
@@ -27,8 +27,8 @@ if (!defined('WPH_CERTIFICATE_VALIDATOR_VERSION')) {define('WPH_CERTIFICATE_VALI
 // adding style and js files for public:
 if (!function_exists('wphcv_plugin_scripts_public')) {
     function wphcv_plugin_scripts_public() {
-    wp_enqueue_style('wphcv-stylesheet', plugins_url('/assets/style.css', __FILE__), '', filemtime(plugin_dir_path(__FILE__).'/assets/style.css'), false);
-    wp_enqueue_script('wphcv-javascript', plugins_url('/assets/script.js', __FILE__), '', filemtime(plugin_dir_path(__FILE__).'/assets/script.js'), true);
+    wp_enqueue_style('wphcv-stylesheet', plugins_url('public/assets/style.css', __FILE__), '', filemtime(plugin_dir_path(__FILE__).'public/assets/style.css'), false);
+    wp_enqueue_script('wphcv-javascript', plugins_url('public/assets/script.js', __FILE__), '', filemtime(plugin_dir_path(__FILE__).'public/assets/script.js'), true);
     }
     add_action('wp_enqueue_scripts', 'wphcv_plugin_scripts_public');
 }
@@ -37,8 +37,8 @@ if (!function_exists('wphcv_plugin_scripts_public')) {
 // adding style and js files for admin panel:
 if (!function_exists('wphcv_plugin_scripts_admin')) {
     function wphcv_plugin_scripts_admin() {
-        wp_enqueue_style('wphcv-stylesheet-admin', plugins_url('/assets/style-admin.css', __FILE__), '', filemtime(plugin_dir_path(__FILE__).'/assets/style-admin.css'), false);
-        wp_enqueue_script('wphcv-javascript-admin', plugins_url('/assets/script-admin.js', __FILE__), '', filemtime(plugin_dir_path(__FILE__).'/assets/script-admin.js'), true);
+        wp_enqueue_style('wphcv-stylesheet-admin', plugins_url('admin/assets/style-admin.css', __FILE__), '', filemtime(plugin_dir_path(__FILE__).'admin/assets/style-admin.css'), false);
+        wp_enqueue_script('wphcv-javascript-admin', plugins_url('admin/assets/script-admin.js', __FILE__), '', filemtime(plugin_dir_path(__FILE__).'admin/assets/script-admin.js'), true);
     }
     add_action('admin_init', 'wphcv_plugin_scripts_admin');
 }
@@ -47,15 +47,15 @@ if (!function_exists('wphcv_plugin_scripts_admin')) {
 // adding top-level menu options to the admin dashboard:
 function wphcv_plugin_admin_dashboard() {
     if (!is_admin()) {return;}
-    else {require plugin_dir_path(__FILE__) . 'validator-form-admin-dashboard.php';}
+    else {require plugin_dir_path(__FILE__) . 'admin/validator-form-admin-dashboard.php';}
 }
 function wphcv_plugin_admin_add() {
     if (!is_admin()) {return;}
-    else {require plugin_dir_path(__FILE__) . 'validator-form-admin-add.php';}
+    else {require plugin_dir_path(__FILE__) . 'admin/validator-form-admin-add.php';}
 }
 function wphcv_plugin_admin_edit() {
     if (!is_admin()) {return;}
-    else {require plugin_dir_path(__FILE__) . 'validator-form-admin-edit.php';}
+    else {require plugin_dir_path(__FILE__) . 'admin/validator-form-admin-edit.php';}
 }
 function wphcv_plugin_menu_register() {
     add_menu_page('Certificate Validator', 'Cert. Validator', 'manage_options', 'certificate-validator-dashboard', 'wphcv_plugin_admin_dashboard', 'dashicons-awards', 30);
@@ -68,7 +68,7 @@ add_action('admin_menu', 'wphcv_plugin_menu_register');
 
 // adding shortcode to output validator-form ui:
 function wphcv_validator_form_ui() {
-    require plugin_dir_path(__FILE__) . 'validator-form-ui.php';
+    require plugin_dir_path(__FILE__) . 'public/validator-form-ui.php';
 }
 function wphcv_validator_form_shortcode_register() {
     add_shortcode('wphcv_validator_form_shortcode', 'wphcv_validator_form_ui'); 
